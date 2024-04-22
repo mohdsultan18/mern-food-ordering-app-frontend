b328f197ac2ac2f9c9edfd2ba5423333fda6f675
Mohd Sultan <132590302+mohdsultan18@users.noreply.github.com>
2024-04-22 13:22:53 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "/app/globals.css";
+import "/globals.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
