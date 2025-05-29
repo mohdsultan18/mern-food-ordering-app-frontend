e112d1f88c82c164fcebac4cd8cfb0223d3ba6d9
Mohd Sultan <sultan.mohd2023@gmail.com>
2025-05-29 09:49:54 +0530

    fix(auth): add offline_access scope to enable refresh tokens in Auth0

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,42 +1,43 @@
-import { AppState, Auth0Provider } from "@auth0/auth0-react";
-import { useNavigate } from "react-router-dom";
+import { AppState, Auth0Provider } from "@auth0/auth0-react"
+import { useNavigate } from "react-router-dom"
 
 type Props = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 
   if (!domain || !clientId || !redirectUri || !audience) {
-    throw new Error("unable to initialise auth");
+    throw new Error("unable to initialise auth")
   }
 
   const onRedirectCallback = (appState?: AppState) => {
-    navigate(appState?.returnTo || "/auth-callback");
-  };
+    navigate(appState?.returnTo || "/auth-callback")
+  }
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      // this code added after getting refresh error only 2 lines added line 29 and 30 
+      // this code added after getting refresh error only 2 lines added line 29 and 30
       useRefreshTokens={true}
       cacheLocation="localstorage"
       authorizationParams={{
         redirect_uri: redirectUri,
         audience,
+        scope: "openid profile email offline_access",
       }}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
     </Auth0Provider>
-  );
-};
+  )
+}
 
-export default Auth0ProviderWithNavigate;
+export default Auth0ProviderWithNavigate
